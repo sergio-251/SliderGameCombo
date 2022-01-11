8545dc120c6e120a294066422b15b6014ee3d39a
EfimovSchool <sergio-251@yandex.ru>
2022-01-11 10:33:24 +0300

    Initial Commit

---
+++ SliderGameCombo/SliderGameComboApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameComboApp.swift
+//  SliderGameCombo
+//
+//  Created by Sergey Efimov on 11.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameComboApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
